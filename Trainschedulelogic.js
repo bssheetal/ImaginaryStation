0269e8970021294122c737a67b67c251268f541a
SheetalSrikumar <bs.sheetal@gmail.com>
2018-11-27 16:53:56 -0800

    added java scriptfile

---
+++ Trainschedulelogic.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCTc-fnY6H2tIITC5G8aY4izVRC_RI2aq0",
+    authDomain: "trainscheduler-992fe.firebaseapp.com",
+    databaseURL: "https://trainscheduler-992fe.firebaseio.com",
+    projectId: "trainscheduler-992fe",
+    storageBucket: "trainscheduler-992fe.appspot.com",
+    messagingSenderId: "898054871422"
+  };
+  firebase.initializeApp(config);
